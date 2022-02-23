0f2904aec45161950dc013f0f55e1b9cbe0349b6
ZakriaQadir <100275770+ZakriaQadir@users.noreply.github.com>
2022-02-23 04:19:23 -0800

    COMMIT CHANGES

---
+++ q6.js
@@ -0,0 +1,9 @@
+const genratedRandomNumber = Math.floor(Math.random() * 10);
+const guessnumber = prompt("Guess the Number: ");
+if(guessnumber==genratedRandomNumber){
+    alert(`You Win.`);
+}
+else{
+    alert(`Not Matched.`);
+}
+© 2022 GitHub, I
---
+++ q7.js
@@ -0,0 +1,11 @@
+const InputArray = [9,43,5,10];
+let found = InputArray.indexOf(10);
+if(found==0){
+    console.log("Found at First Position.");
+}
+else if(found == InputArray.length - 1){
+    console.log("Found at Last Position.");
+}
+else{
+    console.log("Not Found.");
+}
